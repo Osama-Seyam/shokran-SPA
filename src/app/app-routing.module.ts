c0d8a4e6522d82378d9882a8ea8083ea18f89914
osama siam <osamamostafa065@gmail.com>
2024-05-19 20:58:41 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { BrandsComponent } from './brands/brands.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ContactComponent } from './contact/contact.component';
+import { UsingComponent } from './using/using.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path : "using", component: UsingComponent},
+ { path : "categories", component: CategoriesComponent},
+ { path : "brands", component: BrandsComponent},
+ { path : "contact", component: ContactComponent},
+ { path: "about", component: AboutUsComponent},
+ {path: "**", component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientsComponent } from './clients/clients.component';
+import { UsingComponent } from './using/using.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { BrandsComponent } from './brands/brands.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    FooterComponent,
+    ClientsComponent,
+    UsingComponent,
+    AboutUsComponent,
+    CategoriesComponent,
+    BrandsComponent,
+    ContactComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
